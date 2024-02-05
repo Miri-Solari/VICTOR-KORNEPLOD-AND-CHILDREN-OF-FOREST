7b24d102d309933285353de529c543f24f1dd210
Pug-Vanquisher <mikhail.markov.8484@mail.ru>
2024-02-06 02:06:08 +0300

    r3rr

--- Victor Korneplod and Children of Forest/Assets/Scripts/Animals/Chiken.cs
+++ Victor Korneplod and Children of Forest/Assets/Scripts/Animals/Chiken.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Chiken : Animal
 {
+    public Animator animator;
     protected override void OnTriggerEnter(Collider other)
     {
         DMG = 0;
         other.GetComponent<Lizard>().HalfHp();
         base.OnTriggerEnter(other);
+
+        if (_currentLizardcount < MaxLizardCount)
+        {
+            animator.SetBool("attack", true);
+
+            CancelInvoke("ResetAttackAnimation");
+            Invoke("ResetAttackAnimation", 2f);
+        }
+    }
+
+    void ResetAttackAnimation()
+    {
+        animator.SetBool("attack", false);
     }
 }
