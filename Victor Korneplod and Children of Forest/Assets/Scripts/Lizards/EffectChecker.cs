8e724c4d9a698fa5a2ebe9ad4c7c057168e194c5
Miri-Solari <horniret@gmail.com>
2024-02-05 19:28:59 +0300

    1

---
+++ Victor Korneplod and Children of Forest/Assets/Scripts/Lizards/EffectChecker.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EffectChecker : MonoBehaviour
+{
+    [SerializeField] GameObject _lizard;
+    [SerializeField] GameObject FlameEffect;
+    [SerializeField] GameObject SlowEffect;
+    [SerializeField] GameObject HangEffect;
+    [SerializeField] GameObject DistEffect;
+
+    void Update()
+    {
+        if (_lizard.TryGetComponent<Hangover>(out Hangover temp))
+        {
+            HangEffect.SetActive(true);
+        }
+        else HangEffect.SetActive(false);
+
+
+        if (_lizard.TryGetComponent<Slow>(out Slow temp1))
+        {
+            SlowEffect.SetActive(true);
+        }
+        else SlowEffect.SetActive(false);
+
+
+        if (_lizard.TryGetComponent<Distraction>(out Distraction temp2))
+        {
+            DistEffect.SetActive(true);
+        }
+        else DistEffect.SetActive(false);
+
+
+        if (_lizard.TryGetComponent<Flame>(out Flame temp3))
+        {
+            FlameEffect.SetActive(true);
+        }
+        else FlameEffect.SetActive(false);
+
+    }
+}
