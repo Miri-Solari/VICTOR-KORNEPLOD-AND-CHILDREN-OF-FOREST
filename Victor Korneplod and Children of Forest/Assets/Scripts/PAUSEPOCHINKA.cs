5e7a8ec733d551091b326294af1549e87af66d3f
Miri-Solari <horniret@gmail.com>
2024-02-06 07:34:49 +0300

    fhgty

---
+++ Victor Korneplod and Children of Forest/Assets/Scripts/PAUSEPOCHINKA.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class PAUSEPOCHINKA : MonoBehaviour
+{
+    public static bool ISPAUSESOUND = false;
+    public static bool ISPAUSED = false;
+}
--- Victor Korneplod and Children of Forest/Assets/VictoryManager.cs
+++ Victor Korneplod and Children of Forest/Assets/VictoryManager.cs
@@ -16,8 +16,16 @@ public class VictoryManager : MonoBehaviour
         GameObject[] lizards = GameObject.FindGameObjectsWithTag("lizard");
         if (lizards.Length == 0)
         {
+            ENDGAME.EndGame = true;
+            SoundManager.Instance.PlayAttackSound(Random.Range(19, 21));
             Debug.Log("Победа");
-           
+            isVictoryChecked = true;
         }
+    }
+
+    public void Lose()
+    {
+        SoundManager.Instance.PlaySound(Random.Range(17, 19));
+
     }
 }
